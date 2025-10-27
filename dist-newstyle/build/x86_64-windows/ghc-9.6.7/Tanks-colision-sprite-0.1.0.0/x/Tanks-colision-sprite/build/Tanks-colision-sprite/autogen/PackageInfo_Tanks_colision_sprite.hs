1263b4e8b64c8a1259f23499441a8cde396e3cae
Pablo Morón García <moronpablo05@gmail.com>
2025-10-27 21:06:01 +0100

    Add files via upload

---
+++ dist-newstyle/build/x86_64-windows/ghc-9.6.7/Tanks-colision-sprite-0.1.0.0/x/Tanks-colision-sprite/build/Tanks-colision-sprite/autogen/PackageInfo_Tanks_colision_sprite.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -w #-}
+module PackageInfo_Tanks_colision_sprite (
+    name,
+    version,
+    synopsis,
+    copyright,
+    homepage,
+  ) where
+
+import Data.Version (Version(..))
+import Prelude
+
+name :: String
+name = "Tanks_colision_sprite"
+version :: Version
+version = Version [0,1,0,0] []
+
+synopsis :: String
+synopsis = ""
+copyright :: String
+copyright = ""
+homepage :: String
+homepage = ""
